75b8e00f5ae99c455b7f0fcb7244e4d6c50f0c40
Matheus Manzoli <manzoli.contato@gmail.com>
2022-03-19 15:16:33 -0300

    Cores mescladas nos estilos

--- styles.h
+++ styles.h
@@ -1,3 +1,16 @@
+#ifndef styles_h
+#define styles_h
+
+// Cores
+#define RED "\x1B[31m"
+#define GRN "\x1B[32m"
+#define YEL "\x1B[33m"
+#define BLU "\x1B[34m"
+#define MAG "\x1B[35m"
+#define CYN "\x1B[36m"
+#define WHT "\x1B[37m"
+#define RESET "\x1B[0m"
+
 void hr(int style)
 {
     switch (style)
@@ -17,3 +30,5 @@ void hr(int style)
         break;
     }
 }
+
+#endif
